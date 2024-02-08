d2c2f36a8f0c9f3e1e72f0e9fcad4acdd8b941ef
GuilhermeWernke <134728943+GuilhermeWernke@users.noreply.github.com>
2024-02-08 15:44:07 -0300

    Exerc2.php

---
+++ Exerc2.php
@@ -0,0 +1,14 @@
+<?php
+
+    $num = 0;
+    $contagem = 0;
+
+    for($i = 1; $i <= 100; $i++)
+    {
+        $num += $i;
+        $contagem = $i;
+        print $contagem . "\n";
+    }
+    print $num;
+
+?>
