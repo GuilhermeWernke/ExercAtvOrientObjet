8414d69013849ef5585e84a44dab2b9ca335d8ce
GuilhermeWernke <134728943+GuilhermeWernke@users.noreply.github.com>
2024-02-08 15:43:21 -0300

    Exerc1.php

---
+++ Exerc1.php
@@ -0,0 +1,47 @@
+<?php
+
+    $contagem = 0;
+    $opcao = 0;
+
+    while(is_integer($opcao) and !($opcao >= 1 and $opcao <=3))
+    {
+        print"Você quer ocódigo em 1-WHLIE, 2-DO-WHILE, ou 3-FOR \n";
+        $opcao = readline();
+
+        if(is_integer($opcao) and  ! ($opcao >= 1 and $opcao <=3))
+        {
+            print"Valor errado";
+        }
+    }
+
+
+    if($opcao == 1)
+    {
+        while($contagem != 10)
+        {
+
+            $contagem++;
+            print $contagem . "\n";
+
+        }
+    }
+    elseif($opcao == 2)
+    {
+        do
+        {
+
+            $contagem++;
+            print $contagem . "\n";
+
+        }while($contagem != 10);
+    }
+    elseif($opcao == 3)
+    {
+        for($i = 1; $i != 11; $i++)
+        {
+
+            print $i . "\n";
+
+        }
+    }
+?>
